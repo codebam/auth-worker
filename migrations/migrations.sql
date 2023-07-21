3eff08088accf71eab8efbc6dc34fe3b99d39893
Sean Behan <codebam@riseup.net>
2023-07-21 17:32:21 -0400

    update for cookie saving

---
+++ migrations/migrations.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS Users (
+	username TEXT,
+	password TEXT
+);
--- src/router.ts
+++ src/router.ts
@@ -1,39 +1,67 @@
 import { Router } from 'itty-router';
+import { parse } from 'cookie';
 
 const newUser = async (env: Env, username: string, password: string) => {
 	await env.DB.prepare('INSERT INTO Users VALUES (?, ?)').bind(username, password).all().catch(console.error);
 };
 
 const checkUser = async (env: Env, username: string, password: string) => {
-	const { results } = await env.DB.prepare('SELECT * FROM Users WHERE username=? AND password=?').bind(username, password).all();
+	const { results } = await env.DB.prepare('SELECT username FROM Users WHERE username=? AND password=?').bind(username, password).all();
 	if (results.length > 0) return true;
 	return false;
 };
 
 const router = Router();
 
-router.post('/auth/register', async (request, env, _ctx) => {
-	const content = await request.json();
-	const username = content.username;
-	const hash = await crypto.subtle.digest('SHA-512', new TextEncoder().encode(content.password));
-	await newUser(
-		env,
-		username,
-		Array.from(new Uint8Array(hash))
-			.map((b) => b.toString(16).padStart(2, '0'))
-			.join('')
-	);
+const authForm = `<form method="POST" enctype="application/x-www-form-urlencoded">
+			<input type="text" name="username">
+			<input type="password" name="password">
+			<button type="submit">submit</button>
+		</form>`;
+
+router.get('/auth/info', async (request, _env, _ctx) => {
+	const cookie = parse(request.headers.get('Cookie') || '');
+	console.log(cookie);
+	return new Response(JSON.stringify(cookie));
 });
 
-router.post('/auth/login', async (request, _env, _ctx) => {
-	const content = await request.json();
-	const username = content.username;
-	const hash = await crypto.subtle.digest('SHA-512', new TextEncoder().encode(content.password));
+router.post('/auth/register', async (request, env, _ctx) => {
+	const form = await request.formData();
+	const username = form.get('username');
+	const hash = await crypto.subtle.digest('SHA-512', new TextEncoder().encode(form.get('password')));
 	let password = Array.from(new Uint8Array(hash))
 		.map((b) => b.toString(16).padStart(2, '0'))
 		.join('');
+	await newUser(env, username, password);
 	return new Response('ok', {
-		headers: { 'Set-Cookie': `username=${username}; password=${password}` },
+		headers: { 'Set-Cookie': `session=${JSON.stringify({ username, password })}` },
+	});
+});
+
+router.post('/auth/login', async (request, env, _ctx) => {
+	const form = await request.formData();
+	const username = form.get('username');
+	const hash = await crypto.subtle.digest('SHA-512', new TextEncoder().encode(form.get('password')));
+	let password = Array.from(new Uint8Array(hash))
+		.map((b) => b.toString(16).padStart(2, '0'))
+		.join('');
+	if (await checkUser(env, username, password)) {
+		return new Response('ok', {
+			headers: { 'Set-Cookie': `session=${JSON.stringify({ username, password })}` },
+		});
+	}
+	return new Response('invalid login');
+});
+
+router.get('/auth/register', async (_request, _env, _ctx) => {
+	return new Response(authForm, {
+		headers: { 'Content-type': 'text/html' },
+	});
+});
+
+router.get('/auth/login', async (_request, _env, _ctx) => {
+	return new Response(authForm, {
+		headers: { 'Content-type': 'text/html' },
 	});
 });
 
